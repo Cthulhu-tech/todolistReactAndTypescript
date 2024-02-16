abce8c6d1741b74866ff247c4bc0fcc148be40c4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2024-02-16 15:57:53 +0400

    add new start page

---
+++ src/view/startPage/startPage.tsx
@@ -0,0 +1,53 @@
+import { InputForm } from "../../components/InputComponentsGroup/inputComponent/inputComponent";
+import { Button } from "../../components/InputComponentsGroup/buttonComponent/buttonComponent";
+import { InputHandlerType } from "../../components/InputComponentsGroup/inputComponent/type";
+import { updateValue } from "../../store/slices/formSlice/formSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { StoreType } from "../../store/type";
+import { useState } from "react";
+import './startPage.scss';
+
+const StartPage = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const value = useSelector<StoreType, string>((state) => state.inputDataUserName.name);
+
+    const inputValueHandler: InputHandlerType = (
+            event: React.ChangeEvent<HTMLInputElement>
+        ) => dispatch(updateValue(event.target.value));
+
+
+    const actionHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if(loading) return;
+        setLoading((prevState) => prevState = true);
+        setTimeout(() => {
+            navigate('/todo');
+            setLoading((prevState) => prevState = false);
+        }, 1500);
+    }
+
+    return <div className="wrapper-form">
+        <form className="form" onSubmit={actionHandler}>
+            <div className="form__input">
+                <InputForm
+                    placeHolder="You name"
+                    title={`You name: ${value}`}
+                    inputValueHandler={inputValueHandler}
+                    value={value}
+                />
+            </div>
+            <div className="form__button">
+                <Button
+                    title='Submit'
+                    disable={value.length <= 0 || loading}
+                />
+            </div>
+        </form>
+    </div>
+}
+
+export default StartPage;
